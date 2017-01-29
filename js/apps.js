c6fc17eec5d5e603dc70df7686535f5cb5b55019
Gurpal Bahra <gurpal_bahra@hotmail.co.uk>
2017-01-29 19:57:15 +0000

    you win printed when array is empty

--- js/apps.js
+++ js/apps.js
@@ -6,31 +6,31 @@ $(function(){
   var p2WinCounter = 0;
   var player1Area = $("player1");
   var player2Area = $("player2");
-
-
   function callNextKey(){
           for(var i = 0; i<10; i++){
             randomKeys[i] = p1keys[Math.floor(Math.random()*p1keys.length)];
           }
   }
-
   function startButton(){
       var button = $("button");
       button[0].addEventListener("click",function(){
-    })
+      })
       // if(buttonClicked){
       //   return true;
       // }else{
       //   return false;
       // }
-    }
-
+  }
   function keyPress(){
     var keyArea = $("currentKey");
     var cKey = randomKeys[randomKeys.length-1];
+    if(randomKeys.length === 0){
+      currentKey.innerHTML = "you win";
+    }else{
     currentKey.innerHTML = cKey;
+    }
     $(document).keypress(function(event){
-      if((String.fromCharCode(event.which) === cKey) && (randomKeys.length != 0)){
+      if((String.fromCharCode(event.which) === cKey)){
         var indexToSplice = randomKeys.indexOf(randomKeys.length-1);
         randomKeys.splice(indexToSplice,1)
         console.log(randomKeys)
