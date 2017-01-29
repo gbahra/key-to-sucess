74e420658484b1923f45f4dbbe9d4a4413eeb295
Gurpal Bahra <gurpal_bahra@hotmail.co.uk>
2017-01-29 15:40:57 +0000

    different but no better

--- js/apps.js
+++ js/apps.js
@@ -5,55 +5,47 @@ $(function(){
   var p2WinCounter = 0;
   var player1Area = $("player1");
   var player2Area = $("player2");
-
+  var buttonClicked = false;
 
 function callNextKey(){
         var rand = Math.floor(Math.random()*p1keys.length);
         return p1keys[rand] ;
 }
 
-function playButtons(){
+function startButton(){
+  console.log('ads')
     var button = $("button");
-    var keyArea = $("currentKey");
     button[0].addEventListener("click",function(){
-        var cKey = callNextKey();
-        currentKey.innerHTML = cKey;
-        $(document).keypress(function(event){
-        if(String.fromCharCode(event.which) == cKey){
-              alert("yeah");
+      buttonClicked = true;
+  })
+    if(buttonClicked){
+      return true;
+    }else{
+      return false;
+    }
+  }
+
+  function keyPress(){
+    console.log('jsfjsd');
+    var keyArea = $("currentKey");
+    var cKey = callNextKey();
+    currentKey.innerHTML = cKey;
+    console.log("hjdsjhf")
+    $(document).keypress(function(event){
+        if(String.fromCharCode(event.which) != cKey){
+              alert('no');
             }
             else{
-
+              alert('yes')
             }
-
-        });
-        });
+        })
   }
-//         for(var i = 0; i<3; i++){
-//             if(player1[i] === cBall){
-//              p1WinCounter++;
-//              console.log(p1WinCounter)
-//              player1Area.children[i].style.color = "red";
-//             } else if(player2[i] === cBall){
-//                 p2WinCounter++;
-//                 console.log(p2WinCounter)
-//                 player2Area.children[i].style.color = "red";
 
-//             }
-//         }
-//         if (p1WinCounter ==3){
-//             var button = document.getElementById("callNext");
-//             button.style.display = 'none';
-//             player1Area.innerHTML = "p1 winner"
-
-//         } else if(p2WinCounter ==3){
-//             var button = document.getElementById("callNext");
-//             button.style.display = 'none';
-//             player2Area.innerHTML = "p2 winner"
-//         }
-
-// }
-
-playButtons();
+  function runGame(){
+      if(startButton() == true){
+      keyPress();
+    }
+    }
+  runGame();
 
 });
